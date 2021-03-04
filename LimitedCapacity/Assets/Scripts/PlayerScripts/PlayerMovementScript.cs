890fa1f661bf9a22b39a011c5431cf7d877ec307
KaneA23 <55718634+KaneA23@users.noreply.github.com>
2021-03-04 13:58:22 +0000

    Basic player movement
    Player can now walk, run and jump.
    
    Next task is camera follow

---
+++ LimitedCapacity/Assets/Scripts/PlayerScripts/PlayerMovementScript.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controls movement of Player GameObject using 'QWERTY' keyboard
+/// </summary>
+public class PlayerMovementScript : MonoBehaviour {
+	// Physics variables to control movement
+	// Private
+	private bool isGrounded = true; // Can the player jump again?
+	private float speed;
+	// Public
+	public float defaultSpeed = 5.0f;
+	public float runSpeed = 15.0f;
+	public float jumpHeight = 10.0f;
+
+	public Rigidbody rigidBody;
+
+	/// <summary>
+	/// Assign variables to defaults
+	/// </summary>
+	void Start() {
+		rigidBody = gameObject.GetComponent<Rigidbody>();
+		speed = defaultSpeed;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		PlayerMovement();
+	}
+
+	/// <summary>
+	/// When the Player comes in contact with the Floor GameObject, jump is reset
+	/// Checks whether the player can jump
+	/// </summary>
+	/// <param name="collision">The GameObject that has bee triggered by the Player GameObject</param>
+	private void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.tag == "Floor" && !isGrounded) {
+			isGrounded = true;
+		}
+	}
+
+	private void PlayerMovement() {
+		// Sprint mechanic
+		// If the player is pressing the run button, then the speed increases, otherwise default speed
+		if (Input.GetKey(KeyCode.LeftShift)) {
+			speed = runSpeed;
+		} else {
+			speed = defaultSpeed;
+		}
+
+		// Walking mechanics
+		// If the player is pressing a movement button, move in that direction
+		if (Input.GetKey(KeyCode.W))    // Forwards
+		{
+			rigidBody.velocity = new Vector3(rigidBody.velocity.x, rigidBody.velocity.y, speed);
+		}
+		if (Input.GetKey(KeyCode.A))    // Left
+		{
+			rigidBody.velocity = new Vector3(-speed, rigidBody.velocity.y, rigidBody.velocity.z);
+		}
+		if (Input.GetKey(KeyCode.S))    // Backwards
+		{
+			rigidBody.velocity = new Vector3(rigidBody.velocity.x, rigidBody.velocity.y, -speed);
+		}
+		if (Input.GetKey(KeyCode.D))    // Right
+		{
+			rigidBody.velocity = new Vector3(speed, rigidBody.velocity.y, rigidBody.velocity.z);
+		}
+
+		// Jump mechanic
+		// If the player is pressing the jump button and the player is touching the floor, then jump
+		if (Input.GetKey(KeyCode.Space) && isGrounded) {
+			rigidBody.velocity = new Vector3(rigidBody.velocity.x, jumpHeight, rigidBody.velocity.z);
+			isGrounded = false;
+		}
+	}
+}
