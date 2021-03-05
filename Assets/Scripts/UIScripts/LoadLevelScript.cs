1af7865001061e87d0a4610275a297bc6c7ded93
s1802879 <58692827+s1802879@users.noreply.github.com>
2021-03-05 11:40:29 +0000

    Menu

---
+++ Assets/Scripts/UIScripts/LoadLevelScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevelScript : MonoBehaviour {
+
+	public void Levelone() {
+		SceneManager.LoadScene("MainGame");
+	}
+	public void Leveltwo() {
+		SceneManager.LoadScene("MainGameLevel2");
+	}
+	public void Title() {
+		SceneManager.LoadScene("Menu");
+	}
+	public void Exit() {
+		Application.Quit();
+	}
+
+}
